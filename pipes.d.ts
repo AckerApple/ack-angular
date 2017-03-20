a4b8182c9b04f2a4146b5cceb4faf42cbaa0428a
ackerapple <acker.dawn.apple@gmail.com>
2017-03-20 15:37:02 -0400

    try

---
+++ pipes.d.ts
@@ -0,0 +1,26 @@
+export declare class Capitalize {
+    transform(input: any): any;
+}
+export declare class CapitalizeWords {
+    transform(input: any): any;
+}
+export declare class Yesno {
+    transform(input: any): any;
+}
+export declare class YesNo {
+    transform(input: any): any;
+}
+export declare class Numbers {
+    transform(input: any): any;
+}
+export declare class Keys {
+    transform(input: any): string[];
+}
+export declare class TypeofPipe {
+    transform(input: any): "string" | "number" | "boolean" | "symbol" | "undefined" | "object" | "function";
+}
+export declare class ConsolePipe {
+    transform(): any;
+}
+export declare const declarations: typeof Capitalize[];
+export declare function getDeclarations(): typeof Capitalize[];
