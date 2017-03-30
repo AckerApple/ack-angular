e8627b72c9cda98f0d2f0e851798198ae599e5ce
ackerapple <acker.dawn.apple@gmail.com>
2017-03-30 13:50:53 -0400

    0.2.2

--- components.d.ts
+++ components.d.ts
@@ -1,12 +1,21 @@
 import { EventEmitter, ElementRef } from "@angular/core";
+/** adds form element onchange listener via addEventListener('change') that calls formChanged scope argument */
+export declare class FormChanged {
+    element: ElementRef;
+    static parameters: typeof ElementRef[][];
+    onChange: any;
+    formChanged: EventEmitter<{}>;
+    constructor(element: ElementRef);
+    ngOnDestroy(): void;
+}
 export declare class ReaderHeaderBody {
 }
 export declare class ReaderHeader {
 }
 export declare class ReaderBody {
-    el: ElementRef;
+    element: ElementRef;
     static parameters: typeof ElementRef[][];
-    constructor(el: ElementRef);
+    constructor(element: ElementRef);
 }
 export declare class ScreenWidthModel {
     window: any;
@@ -35,4 +44,27 @@ export declare class ScreenHeightModel {
     ngAfterViewInit(): void;
     ngOnDestroy(): void;
 }
-export declare const declarations: (typeof ReaderHeaderBody | typeof ReaderBody)[];
+/** runs shake instructions when condition returns a truthy value */
+export declare class ShakeOn {
+    element: ElementRef;
+    shakeOn: any;
+    shakeThen: EventEmitter<{}>;
+    shakeForMs: any;
+    shakeForMsChange: EventEmitter<{}>;
+    shakeType: any;
+    shakeTypeChange: EventEmitter<{}>;
+    shakeRef: any;
+    shakeRefChange: EventEmitter<{}>;
+    shakeTypes: string[];
+    constructor(element: ElementRef);
+    ngOnInit(): void;
+    ngOnChanges(changes: any): void;
+    onFalse(): void;
+    onTrue(): void;
+}
+/** runs shake instructions when model changes to a truthy value */
+/** runs shake instructions when model changes to a truthy value */
+export declare function hasClass(el: any, className: any): any;
+export declare function addClass(el: any, className: any): void;
+export declare function removeClass(el: any, className: any): void;
+export declare const declarations: (typeof ReaderHeaderBody | typeof ReaderBody | typeof ShakeOn)[];
