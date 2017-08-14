f89301ad93218005d99dc252aef0d65dda6363f8
ackerapple <acker.dawn.apple@gmail.com>
2017-08-14 15:27:26 -0400

    u

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 export { AckModule } from "./AckModule";
 export { declarations as components } from "./components";
 export { declarations as pipes } from "./pipes";
+export declare const ack: any;
 export * from "./providers";
