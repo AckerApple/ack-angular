9d14f138591d9b19a6f5c71b40d18206af37e05d
ackerapple <acker.dawn.apple@gmail.com>
2017-12-05 15:52:19 -0500

    fix folder conflict

--- AckModule.js
+++ AckModule.js
@@ -5,9 +5,9 @@ var common_1 = require("@angular/common");
 var forms_1 = require("@angular/forms");
 var http_1 = require("@angular/http");
 var providers_1 = require("./providers");
-var components_1 = require("./components");
+var declarations_1 = require("./declarations");
 var pipes_1 = require("./pipes");
-var declarations = components_1.declarations.concat(pipes_1.declarations);
+var declarations = declarations_1.declarations.concat(pipes_1.declarations);
 var AckModule = (function () {
     function AckModule() {
     }
